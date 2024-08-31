a840b0a1e103a425ae65cabf191062f1ac9d281c
quezon <gregoryallegory@gmail.com>
2024-08-31 16:02:47 -0400

    Rewrote sh and ps1 files.

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-psql -h localhost -U postgres -d postgres -a -q -f init.sql
+psql -h localhost -U postgres -d postgres -a -q -f init_pg.sql
 python normalizer.py
 python spreadsheet_data_save.py
